7085f61a8394abe4469a0fb75b2cfdb15fadc622
shehandilusanka97 <dilumzsanox9@gmail.com>
2022-09-12 10:02:01 +0530

    commit

--- src/component/hero/Hero.js
+++ src/component/hero/Hero.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
+import { Parallax } from "react-parallax";
+import bg1 from "../images/bg1.jpg";
+import ber from "../images/ber.png";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <Parallax  bgImage={bg1} bgImageAlt="the cat" strength={400}>
+        <img className="img" src={ber} alt="" />
+        <div style={{ height: "500px" }} />
+      </Parallax>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
